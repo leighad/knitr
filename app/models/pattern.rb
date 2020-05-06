0ecab941e54c19073de5052abb62ab5a8166c389
leighad <cdversiontwo@gmail.com>
2020-05-05 17:13:37 -0700

    add has many associations

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -1,3 +1,5 @@
 class Pattern < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :users, through: :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password 
+    has_many :patterns
+    has_many :comments
+    has_many :commented_patterns, through: :comments, source: :pattern 
 end
