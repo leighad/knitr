a86c9f7db35c5a11954e9fb8145de9109078e567
leighad <cdversiontwo@gmail.com>
2020-05-06 09:20:08 -0700

    add create and flash msg

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,17 @@ class SessionsController < ApplicationController
     # def new
     # end
 
+    def create
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id 
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Invalid entry! Please try again."
+            redirect_to login_path
+        end
+    end
+
     def destroy
         session.clear 
         # session.delete :user_id 
