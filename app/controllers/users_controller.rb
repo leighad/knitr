956c959709be3abf68bb16e50cea64612e1c2409
leighad <cdversiontwo@gmail.com>
2020-05-05 18:32:33 -0700

    test signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    private
+
+    # def set_user
+    #     @user = User.find_by_id(params[:user_id])
+    # end
+
+    # def user_params
+    #     params.require(:user).permit(:username, :email, :password)
+    # end
+
+    # def all_users
+    #     User.all 
+    # end
+
+    #play around with all_users to learn more
 end
