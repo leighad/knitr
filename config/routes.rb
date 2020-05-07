d7581f0a956bf695d0daa6be16abb3c989684f8c
leighad <cdversiontwo@gmail.com>
2020-05-07 16:51:27 -0700

    create nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,14 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
 
   resources :comments
-  resources :patterns
-  resources :users
+
+  resources :users do
+    resources :patterns, only: [:new, :create, :index]
+  end
+
+  resources :patterns do 
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
