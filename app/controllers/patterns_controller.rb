e6225f742c2db528bf70171b1125c3e2934bda0c
leighad <cdversiontwo@gmail.com>
2020-05-06 20:14:35 -0700

    set up show

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -5,4 +5,27 @@ class PatternsController < ApplicationController
         @pattern = Pattern.new 
     end
 
+    def create
+        pattern = current_user.patterns.build(pattern_params)
+        if pattern.save
+            redirect_to pattern_path(pattern)
+        else
+            render :new
+        end
+    end
+
+    def show
+        set_pattern
+    end
+
+    private
+
+    def pattern_params
+        params.require(:pattern).permit(:pattern_name, :level, :gauge, :instructions, :notes, :user_id)
+    end
+
+    def set_pattern
+        @pattern = Pattern.find_by_id(params[:id])
+    end
+
 end
