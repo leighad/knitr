a4d0512f219bfdcdec67746b21f0935a586ff3a0
leighad <cdversiontwo@gmail.com>
2020-05-07 15:59:20 -0700

    set up basic controller and private

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,46 @@
 class CommentsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+            redirect_to comments_pat
+        else
+            render :new 
+        end
+    end
+
+    def show
+        set_comment
+    end
+
+    def index
+        @comments = Comment.all 
+    end
+
+    def edit
+        set_comment
+    end
+
+    def update
+        set_comment
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit()
+    end
+
+    def set_comment
+        @comment = Comment.find_by_id(params[:id])
+    end
 end
