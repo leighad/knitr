ea4e1826364c1c65e73db64cbc3b020238d18763
leighad <cdversiontwo@gmail.com>
2020-05-08 22:43:20 -0700

    add validations for pattern_name level instructions

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -2,4 +2,8 @@ class Pattern < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
+
+  validates :pattern_name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :level, presence: true
+  validates :instructions, presence: true
 end
