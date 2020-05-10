8278921f28b55ab5cd05660d9cb231e3b883044c
leighad <cdversiontwo@gmail.com>
2020-05-09 23:08:34 -0700

    add comment for resource comments

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,13 @@ Rails.application.routes.draw do
 
   resources :comments
 
-  resources :users do
-    resources :patterns, only: [:new, :create, :index]
-  end
-
   resources :patterns do 
     resources :comments
+  end
+
+  resources :users do
+    resources :patterns, only: [:new, :create, :index]
+    # resources :comments, shallow: true 
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
