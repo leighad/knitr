a2240747418f1673dabfaec7b9e86fdb68da9cc8
leighad <cdversiontwo@gmail.com>
2020-05-09 21:58:16 -0700

    add error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id 
             redirect_to user_path(@user)
         else
+            flash[:error] = "Must fill out all fields to sign up! Please try again."
             render :new 
         end
     end
