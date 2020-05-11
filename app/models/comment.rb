4d2fc824d73015c80ad440cb3f0152f93dbb8e59
leighad <cdversiontwo@gmail.com>
2020-05-10 18:20:43 -0700

    add scope for alpha and top_rated

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :pattern
 
+  scope :alpha, -> { order (:pattern_name) }
+  scope :top_rated, -> { where ("(rating) = 5") }
+
   validates :rating, presence: true
   validates :content, presence: true, length: { maximum: 80 }
 end
